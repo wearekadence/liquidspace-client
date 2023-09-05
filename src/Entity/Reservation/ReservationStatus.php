2664420a5a4298bc0223086bedad0dd566146e8f
Jordan de Laune <jordan@kadence.co>
2023-09-05 15:21:22 +0100

    Fix lint issues

--- src/Entity/Reservation/ReservationStatus.php
+++ src/Entity/Reservation/ReservationStatus.php
@@ -4,27 +4,27 @@ namespace LiquidSpace\Entity\Reservation;
 
 enum ReservationStatus: int
 {
-    /**
+    /*
      * Cancelled
      */
     case Deleted = 0;
 
-    /**
+    /*
      * Completed (in the past)
      */
     case Completed = 1;
 
-    /**
+    /*
      * Cancelled, but outside the cancellation policy
      */
     case CancellationPolicy = 2;
 
-    /**
+    /*
      * Booked (in the future)
      */
     case Future = 3;
 
-    /**
+    /*
      * Booked, currently within the start and end time
      */
     case Active = 4;
