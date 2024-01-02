8f21f500a0bc41d2f7d3d1f28fd0fa1d6082ace9
Jordan de Laune <jordan@kadence.co>
2024-01-02 10:18:07 +0000

    Check for the presence of properties

--- src/Entity/Venue/Venue.php
+++ src/Entity/Venue/Venue.php
@@ -32,34 +32,78 @@ class Venue
 
     public function __construct(array $venueData)
     {
+        if (!isset($venueData['id'])) {
+            throw new \InvalidArgumentException('Venue data must contain an id');
+        }
         $this->id = $venueData['id'];
+
+        if (!isset($venueData['name'])) {
+            throw new \InvalidArgumentException('Venue data must contain a name');
+        }
         $this->name = $venueData['name'];
+
+        if (!isset($venueData['latitude'])) {
+            throw new \InvalidArgumentException('Venue data must contain a latitude');
+        }
         $this->latitude = $venueData['latitude'];
+
+        if (!isset($venueData['longitude'])) {
+            throw new \InvalidArgumentException('Venue data must contain a longitude');
+        }
         $this->longitude = $venueData['longitude'];
+
+        if (!isset($venueData['address'])) {
+            throw new \InvalidArgumentException('Venue data must contain an address');
+        }
         $this->address = $venueData['address'];
+
+        if (!isset($venueData['simpleAddress1'])) {
+            throw new \InvalidArgumentException('Venue data must contain an address line 1');
+        }
         $this->addressLine1 = $venueData['simpleAddress1'];
         $this->addressLine2 = $venueData['simpleAddress2'];
+
+        if (!isset($venueData['city'])) {
+            throw new \InvalidArgumentException('Venue data must contain a city');
+        }
         $this->city = $venueData['city'];
         $this->county = $venueData['state'];
         $this->postalCode = $venueData['zip'];
         // Convert UK to official ISO 3166-1 alpha-2 code
+        if (!isset($venueData['country'])) {
+            throw new \InvalidArgumentException('Venue data must contain a country code');
+        }
         $this->countryCode = match ($venueData['country']) {
             'UK' => 'GB',
             default => $venueData['country'],
         };
         $this->timeZoneId = $venueData['timeZone']['tzdbIds'][0] ?? 'UTC';
         $this->description = $venueData['description'];
-        $this->imageUrls = $venueData['imageUrls'];
+        $this->imageUrls = $venueData['imageUrls'] ?? [];
+
+        if (!isset($venueData['url'])) {
+            throw new \InvalidArgumentException('Venue data must contain a url');
+        }
         $this->url = $venueData['url'];
+
+        if (!isset($venueData['averageRating'])) {
+            throw new \InvalidArgumentException('Venue data must contain an average rating');
+        }
         $this->averageRating = $venueData['averageRating'];
-        $this->ratingCount = $venueData['ratingCount'];
+        $this->ratingCount = $venueData['ratingCount'] ?? 0;
         $this->healthAndSafetyPolicy = $venueData['healthAndSafetyPolicy'];
-        $this->hasLiquidSpacePro = $venueData['hasLiquidspacePro'];
+        $this->hasLiquidSpacePro = $venueData['hasLiquidspacePro'] ?? false;
+
+        if (!isset($venueData['venueMapImageUrl'])) {
+            throw new \InvalidArgumentException('Venue data must contain a map image url');
+        }
         $this->mapImageUrl = $venueData['venueMapImageUrl'];
 
         $workspaces = [];
-        foreach ($venueData['hourlyWorkspaces'] as $workspaceData) {
-            $workspaces[] = new Workspace($workspaceData);
+        if (isset($venueData['hourlyWorkspaces']) && \is_array($venueData['hourlyWorkspaces'])) {
+            foreach ($venueData['hourlyWorkspaces'] as $workspaceData) {
+                $workspaces[] = new Workspace($workspaceData);
+            }
         }
         $this->workspaces = $workspaces;
     }
