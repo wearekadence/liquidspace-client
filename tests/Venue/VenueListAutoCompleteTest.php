0383d98fdc3dae61d8845213ed27403cd8794ab2
Jordan de Laune <jordan@kadence.co>
2024-03-14 09:25:28 +0000

    Fix tests

--- tests/Venue/VenueListAutoCompleteTest.php
+++ tests/Venue/VenueListAutoCompleteTest.php
@@ -41,7 +41,7 @@ class VenueListAutoCompleteTest extends TestCase
 
         self::assertNotNull($actualResponse);
         self::assertSame('GET', $mockResponse->getRequestMethod());
-        self::assertSame('https://ls-api-dev.azure-api.net/marketplace/api/venues/search?term=search', $mockResponse->getRequestUrl());
+        self::assertSame('https://api.liquidspace.com/marketplace/api/venues/search?term=search', $mockResponse->getRequestUrl());
         self::assertCount(2, $actualResponse->venues);
         self::assertEquals('04637609-c1d5-4848-b34f-8e1ef83de14f', $actualResponse->venues[0]->id);
         self::assertEquals('London venue from bila', $actualResponse->venues[0]->name);
