25a916842e006ceb996fab4d87a4f054694a3557
Chris Gibson <chris@kadence.co>
2024-08-19 15:24:17 +0100

    Test

--- src/Request/GeoSearchRequest.php
+++ src/Request/GeoSearchRequest.php
@@ -82,7 +82,7 @@ class GeoSearchRequest implements RequestInterface
             $providedOptions['minCapacity'] = $this->minCapacity;
         }
 
-        if (null !== $this->reservationLengthMinutes && ReservationMethod::Hourly === $this->reservationMethod) {
+        if (null !== $this->reservationLengthMinutes) {
             $providedOptions['reservationLengthMinutes'] = $this->reservationLengthMinutes;
         }
 
